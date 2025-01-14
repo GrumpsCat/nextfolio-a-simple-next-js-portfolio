38b6ff1b672273a5963f737f03495b799f4c57ca
Grumps Cat <GrumpsCat@users.noreply.github.com>
2025-01-13 23:57:51 -0800

    Update config.ts

--- app/config.ts
+++ app/config.ts
@@ -8,9 +8,8 @@ export const metaData = {
 };
 
 export const socialLinks = {
-//twitter: "https://x.com/1tssirius",
   github: "https://github.com/1msirius/Nextfolio",
   instagram: "https://www.instagram.com/",
   linkedin: "https://www.linkedin.com/",
-//email: "mailto:example@gmail.com",
+email: "mailto:example@gmail.com",
 };
