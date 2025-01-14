44c7a54a19f84ef8f32600bc2ab35630c02c9151
Grumps Cat <GrumpsCat@users.noreply.github.com>
2025-01-13 23:56:44 -0800

    Update config.ts

--- app/config.ts
+++ app/config.ts
@@ -1,6 +1,6 @@
 export const metaData = {
   baseUrl: "https://nextfolio-template.vercel.app/",
-  title: "Gregory1",
+  title: "Gregory",
   name: "Gregory",
   ogImage: "/opengraph-image.png",
   description:
@@ -8,9 +8,9 @@ export const metaData = {
 };
 
 export const socialLinks = {
-twitter: "https://x.com/1tssirius",
+//twitter: "https://x.com/1tssirius",
   github: "https://github.com/1msirius/Nextfolio",
   instagram: "https://www.instagram.com/",
   linkedin: "https://www.linkedin.com/",
-email: "mailto:example@gmail.com",
+//email: "mailto:example@gmail.com",
 };
